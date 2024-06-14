9e53841ee41269a8005514b1b9029d3bfc7bddba
Kevin Santiago Plata <ksantiagoplata@gmail.com>
2024-06-14 17:58:35 -0500

    proceso...

--- Vistas/app_p.js
+++ Vistas/app_p.js
@@ -1,5 +1,6 @@
 let dom = document;
 let main = dom.querySelector('main')
+let side = dom.querySelector('aside.content_aside')
 let checkbox = dom.querySelector('input.check_slider')
 
 checkbox.addEventListener( 'change' , function() {
@@ -9,6 +10,7 @@ checkbox.addEventListener( 'change' , function() {
     }
     else{
         console.log("inactive")
-        main.className = ' '
+        main.className = ''
+
     }
 });         
