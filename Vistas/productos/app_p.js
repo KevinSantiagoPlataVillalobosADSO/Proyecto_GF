f456b7ed1f8a4a73f5670d78472552f891626e90
kevin santiago Plata <ksantiagoplata@gmail.com>
2024-06-21 15:18:43 -0500

    actualizaxion menor c:

---
+++ Vistas/productos/app_p.js
@@ -0,0 +1,25 @@
+let dom = document;
+let main = dom.querySelector('main')
+
+
+
+
+
+
+
+
+
+
+
+// let side = dom.querySelector('aside.content_aside')
+// let checkbox = dom.querySelector('input.check_slider')
+// checkbox.addEventListener( 'change' , function() {
+//     if(this.checked) {
+//         console.log("active")
+//         main.className ='night'
+//     }
+//     else{
+//         console.log("inactive")
+//         main.className = ''
+//     }
+// });         
