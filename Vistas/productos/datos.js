3415a74231eb480e866b5b19cd9272278658c227
KevinAdso <Ksantiagoplata@gmail.com>
2024-06-28 01:20:42 -0500

    actualizacion

--- Vistas/productos/datos.js
+++ Vistas/productos/datos.js
@@ -1,21 +1,63 @@
 export let data = [
     {   
         "nombre": "Manzana",
-        "src": "",
+        "src": "../../IMG/productos/manzanas.webp",
         "precio": "1500/lb",
         "stock": "100Lbs",
         "info_add": "Fecha de vencimineto: 12/04/2025"
     },
     {
         "nombre": "Pera",
-        "src": "",
+        "src": "../../IMG/productos/pera.jpg",
         "precio": "2000/lb",
         "stock": "60Lbs",
         "info_add": "Fecha de vencimineto: 10/06/2025"
     },
     {
         "nombre": "Lulo",
-        "src": "",
+        "src": "../../IMG/productos/lulo.jpg",
+        "precio": "5000/lb",
+        "stock": "20Lbs",
+        "info_add": "Fecha de vencimineto: 23/07/2025"
+    },
+    {
+        "nombre": "cereza",
+        "src": "../../IMG/productos/cereza.jpg",
+        "precio": "5000/lb",
+        "stock": "20Lbs",
+        "info_add": "Fecha de vencimineto: 23/07/2025"
+    },
+    {
+        "nombre": "Sandia",
+        "src": "../../IMG/productos/sandia.jpg",
+        "precio": "5000/lb",
+        "stock": "20Lbs",
+        "info_add": "Fecha de vencimineto: 23/07/2025"
+    },
+    {
+        "nombre": "uvas",
+        "src": "../../IMG/productos/uvas.png",
+        "precio": "5000/lb",
+        "stock": "20Lbs",
+        "info_add": "Fecha de vencimineto: 23/07/2025"
+    },
+    {
+        "nombre": "piña",
+        "src": "../../IMG/productos/piña.jpg",
+        "precio": "5000/lb",
+        "stock": "20Lbs",
+        "info_add": "Fecha de vencimineto: 23/07/2025"
+    },
+    {
+        "nombre": "papaya",
+        "src": "../../IMG/productos/papaya.jpg",
+        "precio": "5000/lb",
+        "stock": "20Lbs",
+        "info_add": "Fecha de vencimineto: 23/07/2025"
+    },
+    {
+        "nombre": "naranja",
+        "src": "../../IMG/productos/naranja.jpg",
         "precio": "5000/lb",
         "stock": "20Lbs",
         "info_add": "Fecha de vencimineto: 23/07/2025"
