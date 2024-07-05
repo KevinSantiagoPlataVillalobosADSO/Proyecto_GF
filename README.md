44683c16a21e960c4b3bc75f438e14cf83a1e94a
KevinAdso <ksantiagoplata@gmail.com>
2024-07-05 13:16:56 -0500

    trabajo de hoy

---
+++ README.md
@@ -0,0 +1,10 @@
+## Installation
+
+***
+A little intro about the installation. 
+```
+$ git clone https://example.com
+$ cd ../path/to/the/file
+$ npm install
+$ npm start
+```
---
+++ Vistas/productos/app_p2.js
@@ -0,0 +1,56 @@
+import {data} from "../productos/datos.js";
+
+(async()=>{
+    let response = await fetch("http://localhost:3000/productos")
+    let datos = await response.json();
+    console.log(datos)
+
+    let dom = document;
+    let $padre = dom.querySelector(".container");
+    let $cards = dom.querySelector(".cards");
+    let fragment = dom.createDocumentFragment();
+
+    datos.map((x) => {
+        let card = dom.createElement("div")
+        card.classList.add("card")
+            let img = dom.createElement("img")
+            img.setAttribute("src", x.src)
+            card.appendChild(img)
+
+            let card_inf = dom.createElement("div")
+            card_inf.classList.add("card__info")
+                let card_par = dom.createElement("div")
+                card_par.classList.add("info_paragraph")
+                    for (let i = 0; i < 3; i++) {
+                        let p = dom.createElement("p")
+                        if (i == 0) {
+                            p.classList.add("card__name");
+                            p.textContent = `Nombre: ${x.nombre}`;
+                        }
+                        else if (i == 1) {
+                            p.classList.add("card__price");
+                            p.textContent = `Precio: ${x.precio}`;
+                        }
+                        else if (i == 2) {
+                            p.classList.add("card__stock");
+                            p.textContent = `Stock: ${x.stock}`;
+                        }
+                        card_par.appendChild(p)
+                    }
+                card_inf.appendChild(card_par)
+            card.appendChild(card_inf)
+
+            let card_pls = dom.createElement("div")
+            card_pls.classList.add("card__plus")
+                let p2 = dom.createElement("p")
+                p2.classList.add("plus__info")
+                p2.textContent = x.info_add
+            card_pls.appendChild(p2)
+        card_inf.appendChild(card_pls)
+        fragment.appendChild(card)
+    })
+
+    $cards.appendChild(fragment)
+})();
+
+
